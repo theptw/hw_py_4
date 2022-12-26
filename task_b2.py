c0702a607e21afc7ce42ddbac6f922ba4cec8e51
Nikita <nikita.bykov485@yandex.com>
2022-12-26 11:34:23 +0500

    vers 2

--- task_b2.py
+++ task_b2.py
@@ -17,52 +17,43 @@ print(var1)
 var1 = var1.replace('+', '')
 var1 = var1.replace('=', '')
 var2 = var2.replace('*', '')
-var1 = var1.replace('0', '')
-print(var1)
 
+print(var1)
 
 var1 = var1.split()
 print(var1)
+del var1[-1]
+print(var1)
 
-koef1 = ''
+koef = ''
 power = ''
-x = 3
+
 for b in range(len(var1)):
-    help_koef1 = koef1
-    
-    koef1 = ''
+    x = 3
+    koef = ''
     power = ''
     for i in range(len(var1[b])):
-        # if var1[b][i] != 'x' and i < x:
-        #     koef1 += var1[b][i]
-        if var1[b][i] == 'x' and len(var1[b]) - len(var1[b][i:]) == 1:
-            koef1 += var1[b][i-1]
-            x = i
-        elif var1[b][i] == 'x' and len(var1[b]) - len(var1[b][i:]) == 2:
-            koef1 += var1[b][i-2] + var1[b][i-1]
-            x = i
-        elif var1[b][i] == 'x' and len(var1[b]) - len(var1[b][i:]) == 0:
-            koef1 = '1'
-            x = i
-        elif i > x:
-            power += var1[b][i]
-        elif var1[b][i] == 'x':
+        if len(var1[b]) == 1:
+            koef = '1'
             power = '1'
-        # elif i > x and len(var1[b]) - i == 1:
-        #     power = var1[b][i]
-        # elif i > x and len(var1[b]) - i == 2:
-        #     power = var1[b][i] + var1[b][i+1]
-        # elif i > x and len(var1[b]) - i == 0:
-        #     power = '1'
-    my_dict[power] = koef1
-        
-        
-        
-        
+        if len(var1[b]) == 2 and var1[b][1] == 'x':
+            koef = var1[b][0]
+            power = '1'
+        elif len(var1[b]) == 2 and var1[b][0] == 'x':
+            power = var1[b][1]
+            koef = '1'
+        if var1[b][i] == 'x':
+            x = i
+            if var1[b][-1] == 'x':
+                power = '1'
+        if var1[b][i] != 'x' and i < x:
+            koef += var1[b][i]
+        elif var1[b][i] != 'x' and i > x and len(var1[b]) > 2:
+            power += var1[b][i]
+    my_dict[power] = koef
+    
 
 
+ndict = {**my_dict}
+print(ndict)
 
-print(my_dict)
-
-# if var1[b][i] == 'x' and len(var1[b]) - (i+1) == 1:
-#             my_dict[var1[b][i+1]] = var1[b][i-1]
