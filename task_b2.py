d04f4a78a750601e40d25a500762e006624c314a
Nikita <nikita.bykov485@yandex.com>
2022-12-27 13:43:38 +0500

    corrections and comments

--- task_b2.py
+++ task_b2.py
@@ -15,48 +15,42 @@ for line in data:
     var2 = line
 data.close()
 
-var1 = var1.replace('*', '')
+var1 = var1.replace('*', '') # формирование списков из многочленов для удобной передачи в словарь
 var1 = var1.replace('+', '')
 var1 = var1.replace('=', '')
 var2 = var2.replace('*', '')
 var2 = var2.replace('+', '')
 var2 = var2.replace('=', '')
-
-print(var1, var2)
-
-
 var1 = var1.split()
 var2 = var2.split()
 del var1[-1]
 del var2[-1]
-print(var1)
-print(var2)
 
-koef = ''
-power = ''
+print(f'Список из первого файла = {var1}')
+print(f'Список из второго файла = {var2}')
 
 def form_dict(list1, list2):
     for b in range(len(list1)):
         x = 3
         koef = ''
         power = ''
-        for i in range(len(list1[b])):
-            if len(list1[b]) == 1:
+        for i in range(len(list1[b])): #перебор вариантов полученных значений и внесение в словарь
+            if len(list1[b]) == 1: # Только X
                 koef = '1'
                 power = '1'
-            if len(list1[b]) == 2 and list1[b][1] == 'x':
+            if len(list1[b]) == 2 and list1[b][1] == 'x': # Число и X после
                 koef = list1[b][0]
                 power = '1'
-            elif len(list1[b]) == 2 and list1[b][0] == 'x':
+            elif len(list1[b]) == 2 and list1[b][0] == 'x': # Х и число после
                 power = list1[b][1]
                 koef = '1'
             if list1[b][i] == 'x':
                 x = i
                 if list1[b][-1] == 'x':
                     power = '1'
-            if list1[b][i] != 'x' and i < x:
+            if list1[b][i] != 'x' and i < x: # Остальные случаи: если i меньше x добавляем цифру в коэфицент
                 koef += list1[b][i]
-            elif list1[b][i] != 'x' and i > x and len(list1[b]) > 2:
+            elif list1[b][i] != 'x' and i > x and len(list1[b]) > 2: # если больше - в степень
                 power += list1[b][i]
         dict1[power] = koef
 
@@ -87,8 +81,6 @@ def form_dict(list1, list2):
         
 print(form_dict(var1,var2))
 
-
-
 def final_str_original_only(dict1,dict2):
     key_difference1 = dict1.keys() - dict2.keys()
     key_difference2 = dict2.keys() - dict1.keys()
@@ -115,16 +107,27 @@ def final_str_original_only(dict1,dict2):
             uni_values.append(dict1[uni_keys[i]])
 
         uni_final = []
-        for i in range(len(uni_values)):
-            uni_final.append(uni_values[i] + '*x**' + uni_keys[i])
+        temp = 0
+        print(uni_keys)
+        print(uni_values)
+        for i in range(len(uni_keys)): # перевод в int для сортировки
+            uni_keys[i] = int(uni_keys[i])
+            uni_values[i] = int(uni_values[i])
+        
+        for run in range(len(uni_keys) - 1): # сортировка
+            for i in range(len(uni_keys) - 1):
+                if uni_keys[i] < uni_keys[i+1]:
+                    uni_keys[i], uni_keys[i+1] = uni_keys[i+1], uni_keys[i]
+                    uni_values[i], uni_values[i+1] = uni_values[i+1], uni_values[i]
+
+        for i in range(len(uni_values)): # формирование финального массива строк уникальных степеней(которые есть только в одном списке)
+            uni_final.append(str(uni_values[i]) + '*x**' + str(uni_keys[i]))
         return uni_final
-
-
 
 final = final_str_original_only(dict1, dict2)
 print(final)
-print(dict1)
-print(dict2)
+
+
 if len(dict2) > len(dict1):
     difference = len(dict2) - len(dict1)
     for i in range(len(dict2), len(dict1), -1):
@@ -133,14 +136,20 @@ elif len(dict2) < len(dict1):
     difference = len(dict1) - len(dict2)
     for i in range(len(dict1), len(dict2), -1):
         dict1.pop(f'{i}')
-    
-print(dict1)
-print(dict2)
-
 for i in range(len(dict1)):
     final.append(str(int(dict1[f'{len(dict1) - i}']) + int(dict2[f'{len(dict1) - i}'])) + '*x**' + f'{len(dict1) - i}')
-
+        
 print(final)
+# maxx = 0
+# temp = 0
+# for i in range(len(final)):
+#     for b in range(len(final[i])):
+#         if final[i][b] == 'x':
+#             x = b
+#         if len(final[i][x:]) == 4 and int(final[i][-2:]) > maxx:
+#             maxx = final[i]
+
+
 
 data = open('sum.txt', 'w')
 for i in range(len(final)):
